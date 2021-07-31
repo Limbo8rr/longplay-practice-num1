842f8b1c474c8d31f512ffcf2df1b4b22279c356
Limbo8rr <77548425+Limbo8rr@users.noreply.github.com>
2021-07-31 14:37:19 -0700

    Update pxt.json, main.blocks, main.ts, images.g.jres

--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping,
+    spinning
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (girl == true) {
+        animation.runImageAnimation(
+        mySpritehead,
+        assets.animation`girl animation`,
+        250,
+        false
+        )
         animation.runImageAnimation(
         mySpritehead,
         assets.animation`girl animation`,
@@ -34,10 +46,12 @@ game.splash("girls and boys actions and thoughts are different")
 game.showLongText("example: girls can use magic, boys can't but are better with a physical attack. girls's regular attack does little damage but before you can cast spells you have to learn them. learn spells by leveling up then press B, when pressing B, you will get 2 options, menu and magic, magic list the magic you know so you can use them. menu opensthe menu where you can use items, change equipment, and learn about the magic you know, and show this guide. use A to use a normal attack.and that is all this guide possesses.", DialogLayout.Full)
 if (game.ask("girl", "boy")) {
     girl = true
+    info.setLife(110)
     mySpritehead = sprites.create(assets.image`girl Head`, SpriteKind.Player)
     mySpritefeet = sprites.create(assets.image`girlBottom`, SpriteKind.Player)
 } else {
     girl = false
+    info.setLife(160)
     mySpritehead = sprites.create(assets.image`boy`, SpriteKind.Player)
 }
 mySpritehead.setFlag(SpriteFlag.GhostThroughWalls, true)
